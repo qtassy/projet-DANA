c2aad62f06af992a5240b8a59b2b19ec63b4b961
Quentin Tassy <tassyquentin.17@gmail.com>
2021-03-10 09:39:32 +0100

    Amélioration visuelle du composant card du module accueil

--- homemodule/src/home.js
+++ homemodule/src/home.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Icon from "./img/icon-home.png";
+import React from 'react';
+import Icon1 from "./img/icon1-home.png";
+import Icon2 from "./img/icon2-home.png";
 import './index.css';
+import { IoIosArrowForward } from "react-icons/io";
 
 class Card extends React.Component{
     render(){
         return(
             <div className="col-xs-12 col-sm-6 col-md-4 mb-5">
-                <a href="#" className="link-card">
-                    <div className="card h-100 text-center">
-                        <div className="card-body home-card">
-                            <img src={Icon}/>
-                            <h5 className="card-title fw-bold text-light">{this.props.title_card}</h5>
-                            <p className="card-text text-light">{this.props.text_card}</p>
-                        </div>
+                <div className="card h-100 text-center">
+                    <div className="card-body home-card">
+                        <a href={this.props.link} className="link-card">
+                            <div className="round">
+                                <IoIosArrowForward className="arrow"/>
+                            </div>
+                        </a>
+                        <img src={this.props.icon} alt="icon"/>
+                        <h5 className="card-title font-weight-bold text-light">{this.props.title_card}</h5>
+                        <p className="card-text text-light">{this.props.text_card}</p>
                     </div>
-                </a>
+                </div>
             </div>
         )
     }
@@ -26,21 +30,38 @@ class Home extends React.Component{
         return(
             <>
                 <div className="container">
+                    <h1 className="title-home mb-3">Tableau de bord</h1>
                     <div className="row">
                             <Card 
+                                icon={Icon1}
                                 title_card="Taches administratives" 
                                 text_card='On vous accompagne pour vous faciliter la vie avec la "paperasse"'
+                                link="\#"
                             />
                             <Card 
+                                icon={Icon2}
                                 title_card="Gestion des contrats" 
                                 text_card="Laisser-vous guider pour transférer ou souscrire à un contrat"
+                                link="\#"
                             />
                             <Card 
+                                icon={Icon2}
                                 title_card="Tri de ses affaires" 
                                 text_card="Vous ne savez plus ou donner de la tête ? Trier, donner, jeter ?"
+                                link="\#"
                             />
-                            <Card title_card="Faire ses cartons" text_card="Vous ne savez plus ou donner de la tête ?"/>
-                            <Card title_card="Le transport" text_card="Vous ne savez plus ou donner de la tête ?"/>
+                            <Card 
+                                icon={Icon2}
+                                title_card="Faire ses cartons" 
+                                text_card="Vous ne savez plus ou donner de la tête ?"
+                                link="\#"
+                            />
+                            <Card 
+                                icon={Icon2}
+                                title_card="Le transport" 
+                                text_card="Vous ne savez plus ou donner de la tête ?"
+                                link="\#"
+                            />
                         </div>
                     </div>
             </> 
--- homemodule/src/index.js
+++ homemodule/src/index.js
@@ -1,40 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Menu from './menu.js';
 import Home from './home.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-
-import Logo from "./img/logo1_hori.jpg"
-import { BsFillPersonFill } from "react-icons/bs";
-import { AiFillShopping } from "react-icons/ai";
-import { RiMessage2Fill } from "react-icons/ri";
-import { AiOutlineMenuUnfold } from "react-icons/ai";
-
-class Menu extends React.Component{
-  render(){
-      return(
-          <nav className="navbar navbar-expand-lg navbar-dark nav-home mb-5">
-              <a className="navbar-brand" href="#">
-                  <img src={Logo} alt="logo principal Dana" width="150"/>
-              </a>
-              <ul className="navbar-nav">
-                  <li className="nav-item">
-                      <a className="nav-link active" href="#"><BsFillPersonFill/>Profil</a>
-                  </li>
-                  <li className="nav-item">
-                      <a className="nav-link" href="#"><AiFillShopping/>Promos</a>
-                  </li>
-                  <li className="nav-item">
-                      <a className="nav-link" href="#"><RiMessage2Fill/>Messages</a>
-                  </li>
-                  <li className="nav-item">
-                      <a className="nav-link" href="#"><AiOutlineMenuUnfold/>Plus</a>
-                  </li>
-              </ul>
-          </nav>
-      )
-  }
-}
 
 ReactDOM.render(
   <React.StrictMode>
