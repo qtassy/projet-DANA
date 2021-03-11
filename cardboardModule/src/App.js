410fb5e156230efda1448cf0615966a85df89686
Damien <dnoeldev@gmail.com>
2021-03-11 14:40:57 +0100

    CreateCardBoardComponent and redirection to it. Code cleaning

--- cardboardModule/src/App.js
+++ cardboardModule/src/App.js
@@ -4,7 +4,7 @@ import { Route, Redirect} from "react-router-dom";
 import MakeMyCardboards from './pages/MakeMyCardboards/MakeMyCardboards';
 import Home from './pages/Home/Home';
 import MyCardboards from './pages/MyCardboards/MyCardboards';
-
+import CreateCardboard from './pages/CreateCardboard/CreateCardboard'
 const App = () => (
   <BrowserRouter>
     <Route exact path="/">
@@ -14,6 +14,7 @@ const App = () => (
 
     <Route exact path="/MakeMyCardboards" component={MakeMyCardboards} />
       <Route exact path="/MakeMyCardboards/myCardBoards" component={MyCardboards} />
+        <Route exact path="/MakeMyCardboards/CreateCardboard" component={CreateCardboard} />
 
   </BrowserRouter>
 );
---
+++ cardboardModule/src/pages/CreateCardboard/CreateCardboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './CreateCardboard.scss';
+const CreateCardboard = () => (
+  <div >
+    CreateCardboard Component
+  </div>
+);
+
+export default CreateCardboard;
