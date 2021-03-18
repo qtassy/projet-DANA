0d4064894d02c4ce7288a332a3ce12107e602e32
Stoliaroff <clem.sto@orange.fr>
2021-03-18 08:28:00 +0100

    final label

--- dana_label/src/App.js
+++ dana_label/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import CreationCompte from './component/CreationCompte/CreationCompte';
+import CreationCompteClient from './component/CreationCompte/CreationCompteClient';
+import CreationComptePro from './component/CreationCompte/CreationComptePro';
 import { CreationPieces } from './component/CreationPieces/CreationPieces';
 import FormulaireConnaissance from './component/CreationProjetDemenagement/FormulaireConnaissance';
 import { ListeEtiquettes } from './component/Etiquettes/ListeEtiquettes.js';
@@ -11,7 +12,8 @@ function App() {
       <Route exact path="/" component={ListeEtiquettes} />
       <Route exact path="/ListeEtiquettes" component={ListeEtiquettes} />
       <Route exact path="/FormulaireConnaissance" component={FormulaireConnaissance} />
-      <Route exact path="/CreationCompte" component={CreationCompte} />
+      <Route exact path="/CreationCompteClient" component={CreationCompteClient} />
+      <Route exact path="/CreationComptePro" component={CreationComptePro} />
       <Route exact path="/CreationPieces" component={CreationPieces} />
     </BrowserRouter>
   );
