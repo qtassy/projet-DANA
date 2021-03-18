eabeeea90cbb82e6ca44769bdb01846d278aae0a
Damien <dnoeldev@gmail.com>
2021-03-18 10:09:48 +0100

    begining of integration

--- projet-dana/src/App.js
+++ projet-dana/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login/Login';
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import Home from './pages/Home/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/home" component={Home} />
+
+      {/* Module MakeMyCardboards */}
+      {/* <Route exact path="/MakeMyCardboards" component={MakeMyCardboards} />
+      <Route exact path="/MakeMyCardboards/myCardBoards" component={MyCardboards} />
+      <Route exact path="/MakeMyCardboards/myCardBoards/createCardboard" component={CreateCardboard} />
+      <Route exact path="/MakeMyCardboards/myCardBoards/room" component={ListCardboardByRoom} /> */}
+
+      {/* Module Transport */}
+      {/* <Route exact path="/Transport" component={Transport} /> */}
+
+  </BrowserRouter>
   );
 }
 
